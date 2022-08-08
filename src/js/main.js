e0d86ed0100a4d1e3685272423b3049704522b66
Pinalphine <a4cfee5c3de7d@yahoo.com>
2022-08-08 11:47:29 +0000

    fixed navigation bug for btns mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 const menuBar = document.getElementById('menu-bar');
 const navLink = document.getElementById('nav-link');
+const menuBtn = document.getElementById('menu-btn');
 
 menuBar.addEventListener('click', ()  => {
     navLink.classList.toggle('show-menu')
+    menuBtn.classList.toggle('show-menu')
 });
