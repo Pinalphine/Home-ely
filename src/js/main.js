89cf986cab2863b05b7e40556849315b9f9d9817
Pinalphine <a4cfee5c3de7d@yahoo.com>
2022-08-26 11:45:05 +0000

    finished nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,21 @@
-const menuBar = document.getElementById('menu-bar');
-const navLink = document.getElementById('nav-link');
-const menuBtn = document.getElementById('menu-btn');
+const navToggle = document.getElementById('nav-toggle'),
+navMenu = document.getElementById('nav-menu'),
+navClose = document.getElementById('nav-close');
 
-menuBar.addEventListener('click', ()  => {
-    navLink.classList.toggle('show-menu')
-    menuBtn.classList.toggle('show-menu')
-});
+// menuBar.addEventListener('click', ()  => {
+//     navLink.classList.add('show-menu')
+//     menuBtn.classList.add('show-menu')
+//     closeBar.classList.remove('show-menu')
+// });
+
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
